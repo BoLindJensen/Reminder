7faa021e2bd603fee810bfe13d492d18e7bf68a6
Bo Lind Jensen <bolind@gmail.com>
2018-10-28 17:23:00 +0100

    Update Python_Code/Snippets/Classes/add_to_list.py

---
+++ Python_Code/Snippets/Classes/add_to_list.py
@@ -0,0 +1,16 @@
+list_of_names = []
+
+
+class PersonClass:
+    def add_person(self, name, name_id=1):
+        name_dic = {"name": name, "name_id": name_id}
+        list_of_names.append(name_dic)
+    pass
+
+person_Instance = PersonClass()
+
+person_Instance.add_person("Bo")  # Assigning a number is optional.
+person_Instance.add_person("Lise", 123)
+
+
+print(list_of_names)
