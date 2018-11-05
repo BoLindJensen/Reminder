bad33ae68ea03dfc875ccbc8d314f90a58cbaca9
Bo Lind Jensen <bolind@gmail.com>
2018-11-05 11:14:10 +0100

    Swap numbers

---
+++ C#/Swap_Numbers/Swap_Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempVarSwap swaps = new TempVarSwap();
+
+            swaps.SwapIntsUsingSelfe(5,10);
+            Console.WriteLine("");
+            swaps.SwapIntsUsingBufferVariable(5,10);
+
+            
+            //Pause Console
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/Swap_Numbers/Swap_Numbers/TempVarSwap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Numbers
+{
+    class TempVarSwap
+    {
+
+        public void SwapIntsUsingBufferVariable(int a, int b) { 
+//            int a = 5;
+//            int b = 10;
+            int c = 0;
+
+            Console.WriteLine("Perform Swap of A & B using C variable: ");
+
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+            Console.WriteLine("C: " + c);
+            Console.WriteLine("Swapping");
+
+            c = b;  // Temp save
+            b = a;  // B becomes A
+            a = c;  // A becomes B via C
+            c = 0;
+
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+            Console.WriteLine("C: " + c);
+        }
+
+
+        public void SwapIntsUsingSelfe(int a, int b){
+            //            int a = 15;
+            //            int b = 5;
+            Console.WriteLine("Perform Swap of A & B using only A & B variables");
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+            Console.WriteLine("Swapping");
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+        }
+
+
+    }
+    
+}
